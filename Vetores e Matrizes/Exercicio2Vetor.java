882d78057c8db0175a4f744697949d036be5ef40
flrvia <flavixbatista@gmail.com>
2022-09-12 21:06:31 -0300

    Adicionando exercícios de matrizes e vetores

--- Vetores e Matrizes/Exercicio2Vetor.java
+++ Vetores e Matrizes/Exercicio2Vetor.java
@@ -14,13 +14,13 @@ public class Exercicio2Vetor {
 
 	public static void main(String[] args) {
 		
-		float[] vetor = new float[6];
+		int[] vetor = new int[6];
 		int contPar=0, somaPar = 0, contImpar=0,x;
 		
 		Scanner leia = new Scanner(System.in);
 		
 		for(x=0;x<6;x++) {
-			System.out.println("Digite um número: ");
+			System.out.println("\nDigite um número: ");
 			vetor[x] = leia.nextInt();
 			
 			if (vetor[x] % 2 == 0) {
@@ -32,20 +32,20 @@ public class Exercicio2Vetor {
 			
 			
 		}
-		System.out.println("Pares digitados: ");
+		System.out.print("\nPares digitados: ");
 		for (x=0;x<6;x++) {
 			if(vetor[x] % 2 == 0) {
-				System.out.println(vetor[x]);
+				System.out.print("["+vetor[x]+"]");
 			}
 		}	
-			System.out.println("Ímpares digitados: ");
+			System.out.print("\nÍmpares digitados: ");
 			for(x=0;x<6;x++) {
 			if (vetor[x] % 2 != 0) {
-				System.out.println(vetor[x]);
+				System.out.print("["+vetor[x]+"]");
 			}
 		
 		}
-			System.out.println("\nFoi digitado "+contPar+" números pares.");
+			System.out.println("\n\nFoi digitado "+contPar+" números pares.");
 			System.out.println("\nA soma dos números pares digitado é: "+somaPar);
 			System.out.println("\nFoi digitado "+contImpar+" números ímpares.");
 	
